a4a3c32f2a91e36f585a177d86f25c7cda304072
gong023 <gon.gong.gone@gmail.com>
2017-11-05 23:12:39 -0800

    update package

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/local/go/bin/go get github.com/gong023/my-slack-process...
+/usr/local/go/bin/go get -u github.com/gong023/my-slack-process...
 packages=(stdpost forecast inoreader)
 for package in "${packages[@]}"; do
     /usr/local/go/bin/go install github.com/gong023/my-slack-process/"$package"
