583f872619c65db28c6b68d95a0bd088011c44cb
gong023 <gon.gong.gone@gmail.com>
2017-11-05 23:01:23 -0800

    inoreader

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /usr/local/go/bin/go get github.com/gong023/my-slack-process...
-packages=(stdpost forecast)
+packages=(stdpost forecast inoreader)
 for package in "${packages[@]}"; do
     /usr/local/go/bin/go install github.com/gong023/my-slack-process/"$package"
 done
