5a15a5209cc55bc6d12d7b527214b2b9dbdc40cd
gong023 <gon.gong.gone@gmail.com>
2017-11-12 12:39:00 -0800

    add stdpostb

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /usr/local/go/bin/go get -u github.com/gong023/my-slack-process...
-packages=(stdpost forecast inoreader)
+packages=(stdpost stdpostb forecast inoreader)
 for package in "${packages[@]}"; do
     /usr/local/go/bin/go install github.com/gong023/my-slack-process/"$package"
 done
