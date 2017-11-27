e8b55c5629304e181fb2cc9e591ba77cdb89f8ec
gong023 <gon.gong.gone@gmail.com>
2017-11-26 16:56:16 -0800

    stdpostc

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /usr/local/go/bin/go get -u github.com/gong023/my-slack-process...
-packages=(stdpost stdpostb forecast inoreader coinbase pixiv gistup)
+packages=(stdpost stdpostb stdpostc forecast inoreader coinbase pixiv gistup)
 for package in "${packages[@]}"; do
     /usr/local/go/bin/go install github.com/gong023/my-slack-process/"$package"
 done
---
+++ stdpostc/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gong023/my-slack-process/slack"
+)
+
+func main() {
+	msgs := flag.String("messages", "", "messages")
+	webhook := flag.String("webhook", "", "slack incoming webhook url")
+	flag.Parse()
+	if *msgs == "" || *webhook == "" {
+		log.Fatal("missing parameter")
+	}
+
+	f, err := os.Open(*msgs)
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, msg := range strings.Split(string(b), "\n") {
+		if msg == "" {
+			continue
+		}
+		attachments := slack.Attachments{}
+		err := json.Unmarshal([]byte(msg), &attachments)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := post(*webhook, attachments); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func post(webhook string, attachment slack.Attachments) (err error) {
+	sm, err := json.Marshal(attachment)
+
+	if err != nil {
+		return
+	}
+
+	res, err := http.Post(webhook, "application/json", bytes.NewBuffer(sm))
+
+	if err != nil {
+		return
+	}
+
+	if res.StatusCode >= 300 {
+		return errors.New("fail to post slack:" + res.Status)
+	}
+
+	return
+}
