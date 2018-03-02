9ff1e9952d773122e7856a7e6bad9c65feb012a4
gong023 <gon.gong.gone@gmail.com>
2018-03-01 21:16:20 -0800

    update ci

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /usr/local/go/bin/go get -u github.com/gong023/my-slack-process...
-packages=(stdpost stdpostb stdpostc forecast inoreader coinbase pixiv gistup)
+packages=(stdpost stdpostb stdpostc forecast inoreader coinbase pixiv gistup hibiki)
 for package in "${packages[@]}"; do
     /usr/local/go/bin/go install github.com/gong023/my-slack-process/"$package"
 done
