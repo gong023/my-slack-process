d7360b866517b6f4bc009468cb9e79f92ac90679
gong023 <gon.gong.gone@gmail.com>
2018-05-18 15:24:08 -0700

    delete server/v1

--- server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/local/go/bin/go get -u github.com/gong023/my-slack-process...
-/usr/local/go/bin/go install github.com/gong023/my-slack-process/server
-/usr/local/go/bin/go install github.com/gong023/my-slack-process/server/reverseproxy
