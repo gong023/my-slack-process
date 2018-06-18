a72a1bb8386ab4a7985bc3ed8b47ff7af8d8712b
gong023 <gon.gong.gone@gmail.com>
2018-06-17 19:41:14 -0700

    refactor hibiki

---
+++ hibiki/client.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+const (
+	ua        = "Mozilla/5.0 (iPad; CPU OS 11_1 like Mac OS X) AppleWebKit/604.3.5 (KHTML, like Gecko) Mobile/15B93 BushiroadMusic(com.bushiroad.HibikiRadio)/2.1.6"
+	host      = "https://vcms-api.hibiki-radio.jp"
+	appVer    = "20106"
+	appBundle = "com.bushiroad.HibikiRadio"
+)
+
+type (
+	Auth struct {
+		ID          int    `json:"id"`
+		AccessToken string `json:"access_token"`
+	}
+
+	FavRes struct {
+		Program Program
+	}
+
+	Program struct {
+		Name              string  `json:"name"`
+		ID                int     `json:"id"`
+		LatestEpisodeName string  `json:"latest_episode_name"`
+		Episode           Episode `json:"episode"`
+	}
+
+	Episode struct {
+		ID              int    `json:"id"`
+		Video           Video  `json:"video"`
+		AdditionalVideo Video  `json:"additional_video"`
+		UpdatedAt       string `json:"updated_at"`
+	}
+
+	Video struct {
+		ID int `json:"id"`
+	}
+
+	PlayCheck struct {
+		Token       string
+		PlaylistURL string `json:"playlist_url"`
+	}
+)
+
+type Session struct {
+	auth Auth
+}
+
+func (s *Session) Start(email, password string) error {
+	data := url.Values{}
+	data.Add("email", email)
+	data.Add("password", password)
+	body := bytes.NewReader([]byte(data.Encode()))
+	req, err := http.NewRequest("POST", host+"/api/v1/users/auth", body)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	auth := Auth{}
+	err = s.do(req, &auth)
+	if err != nil {
+		return err
+	}
+	s.auth = auth
+
+	return nil
+}
+
+func (s *Session) Favorites() (fav []FavRes, err error) {
+	req, err := http.NewRequest("GET", host+"/api/v1/user_favorites", nil)
+	if err != nil {
+		return
+	}
+	err = s.do(req, &fav)
+	return
+}
+
+func (s *Session) PlayCheck(video Video) (playCheck PlayCheck, err error) {
+	videoID := strconv.Itoa(video.ID)
+	req, err := http.NewRequest("GET", host+"/api/v1/videos/play_check?video_id="+videoID, nil)
+	if err != nil {
+		return
+	}
+	err = s.do(req, &playCheck)
+	return
+}
+
+func (s *Session) do(req *http.Request, res interface{}) error {
+	req.Header.Add("User-Agent", ua)
+	req.Header.Add("X-BushiroadMusic-App-Version", appVer)
+	req.Header.Add("X-BushiroadMusic-PackageIdentifier", appBundle)
+	req.Header.Add("X-BushiroadMusic-Os", "ipad")
+	req.Header.Add("X-Hibiki-User-Id", strconv.Itoa(s.auth.ID))
+	req.Header.Add("user-id", strconv.Itoa(s.auth.ID))
+	req.Header.Add("X-Hibiki-Access-Token", s.auth.AccessToken)
+
+	client := &http.Client{}
+	r, err := client.Do(req)
+	if err != nil {
+		return nil
+	}
+	defer r.Body.Close()
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil
+	}
+	if r.StatusCode >= 300 {
+		return fmt.Errorf("%s error [%s]:(%s)", req.URL.Path, r.Status, string(b))
+	}
+
+	return json.Unmarshal(b, res)
+}
--- hibiki/main.go
+++ hibiki/main.go
@@ -1,61 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"golang.org/x/sync/errgroup"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
 	"os/exec"
-	"strconv"
 	"time"
-)
-
-const (
-	ua        = "Mozilla/5.0 (iPad; CPU OS 11_1 like Mac OS X) AppleWebKit/604.3.5 (KHTML, like Gecko) Mobile/15B93 BushiroadMusic(com.bushiroad.HibikiRadio)/2.1.6"
-	host      = "https://vcms-api.hibiki-radio.jp"
-	appVer    = "20106"
-	appBundle = "com.bushiroad.HibikiRadio"
-)
-
-type (
-	Auth struct {
-		ID          int    `json:"id"`
-		AccessToken string `json:"access_token"`
-	}
-
-	FavRes struct {
-		Program Program
-	}
-
-	Program struct {
-		Name              string  `json:"name"`
-		ID                int     `json:"id"`
-		LatestEpisodeName string  `json:"latest_episode_name"`
-		Episode           Episode `json:"episode"`
-	}
-
-	Episode struct {
-		ID              int    `json:"id"`
-		Video           Video  `json:"video"`
-		AdditionalVideo Video  `json:"additional_video"`
-		UpdatedAt       string `json:"updated_at"`
-	}
-
-	Video struct {
-		ID int `json:"id"`
-	}
-
-	PlayCheck struct {
-		Token       string
-		PlaylistURL string `json:"playlist_url"`
-	}
 )
 
 func main() {
@@ -70,12 +22,13 @@ func main() {
 	}
 	since := time.Now().Add(*(s) * -1)
 
-	auth, err := login(email, password)
+	session := &Session{}
+	err := session.Start(email, password)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	favs, err := getFavorites(auth)
+	favs, err := session.Favorites()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -97,7 +50,7 @@ func main() {
 
 			localFiles := make(map[string]PlayCheck)
 
-			playCheck, err := getPlayCheck(auth, episode.Video)
+			playCheck, err := session.PlayCheck(episode.Video)
 			if err != nil {
 				return err
 			}
@@ -105,7 +58,7 @@ func main() {
 			localFiles[filePath] = playCheck
 
 			if fav.Program.Episode.AdditionalVideo.ID != 0 {
-				playCheck, err := getPlayCheck(auth, fav.Program.Episode.AdditionalVideo)
+				playCheck, err := session.PlayCheck(fav.Program.Episode.AdditionalVideo)
 				if err != nil {
 					return err
 				}
@@ -130,96 +83,7 @@ func main() {
 	}
 }
 
-func login(email, password string) (auth Auth, err error) {
-	data := url.Values{}
-	data.Add("email", email)
-	data.Add("password", password)
-	body := bytes.NewReader([]byte(data.Encode()))
-	req, err := http.NewRequest("POST", host+"/api/v1/users/auth", body)
-	if err != nil {
-		return
-	}
-	commonHeader(req.Header, auth)
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	client := &http.Client{}
-	res, err := client.Do(req)
-	if err != nil {
-		return
-	}
-	defer res.Body.Close()
-	b, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return
-	}
-	if res.StatusCode >= 300 {
-		return auth, errors.New(fmt.Sprintf("login error [%s]:(%s)", res.Status, string(b)))
-	}
-
-	err = json.Unmarshal(b, &auth)
-	return
-}
-
-func getFavorites(auth Auth) (favs []FavRes, err error) {
-	req, err := http.NewRequest("GET", host+"/api/v1/user_favorites", nil)
-	if err != nil {
-		return
-	}
-	commonHeader(req.Header, auth)
-
-	client := &http.Client{}
-	res, err := client.Do(req)
-	if err != nil {
-		return
-	}
-	defer res.Body.Close()
-	b, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return
-	}
-	if res.StatusCode >= 300 {
-		return favs, errors.New(fmt.Sprintf("user_favorites error [%s]:(%s)", res.Status, string(b)))
-	}
-	err = json.Unmarshal(b, &favs)
-	return
-}
-
-func getPlayCheck(auth Auth, video Video) (playCheck PlayCheck, err error) {
-	videoID := strconv.Itoa(video.ID)
-	req, err := http.NewRequest("GET", host+"/api/v1/videos/play_check?video_id="+videoID, nil)
-	if err != nil {
-		return
-	}
-	commonHeader(req.Header, auth)
-
-	client := &http.Client{}
-	res, err := client.Do(req)
-	if err != nil {
-		return
-	}
-	defer res.Body.Close()
-	b, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return
-	}
-	if res.StatusCode >= 300 {
-		return playCheck, errors.New(fmt.Sprintf("play_check error [%s]:(%s)", res.Status, string(b)))
-	}
-	err = json.Unmarshal(b, &playCheck)
-	return
-}
-
 func ffmpeg(m3u8URL, saveFile string) (err error) {
 	args := []string{"-y", "-vn", "-i", m3u8URL, "-acodec", "copy", "-bsf:a", "aac_adtstoasc", saveFile}
 	return exec.Command("ffmpeg", args...).Run()
-}
-
-func commonHeader(h http.Header, auth Auth) {
-	h.Add("User-Agent", ua)
-	h.Add("X-BushiroadMusic-App-Version", appVer)
-	h.Add("X-BushiroadMusic-PackageIdentifier", appBundle)
-	h.Add("X-BushiroadMusic-Os", "ipad")
-	h.Add("X-Hibiki-User-Id", strconv.Itoa(auth.ID))
-	h.Add("user-id", strconv.Itoa(auth.ID))
-	h.Add("X-Hibiki-Access-Token", auth.AccessToken)
 }
