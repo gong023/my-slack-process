785fcaba106090c3125f7e7710f0d2f47924f193
gong023 <gon.gong.gone@gmail.com>
2018-11-23 18:20:23 -0800

    package variable as storage

--- twitterf/main.go
+++ twitterf/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/gong023/my-slack-process/slack"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
 	"time"
@@ -34,28 +37,65 @@ func main() {
 	if consumerSecret == "" {
 		log.Fatal("consumerKey secret is empty")
 	}
+	webhook := os.Getenv("WEBHOOK")
+	if webhook == "" {
+		log.Fatal("webhook is empty")
+	}
 	cli = anaconda.NewTwitterApiWithCredentials(accessToken, accessSecret, consumerKey, consumerSecret)
 
-	mediaMin := flag.Int("media_min", 2000, "")
-	mediaMax := flag.Int("media_max", 3000, "")
+	mediaMin := flag.Int("media_min", 300, "")
+	mediaMax := flag.Int("media_max", 5000, "")
 	tolerance := flag.Int("tolerance", 3, "for minor")
 	activeTweet := flag.Duration("active_tweet", -3*24*time.Hour, "")
+	interval := flag.Duration("interval", 15*time.Minute, "")
 	flag.Parse()
 
+	out, err := process(*mediaMin, *mediaMax, *tolerance, *activeTweet)
+	if err != nil {
+		out = []slack.Attachment{
+			{
+				Text: err.Error(),
+			},
+		}
+	}
+	if err := postSlack(webhook, slack.Attachments{Attachments: out}); err != nil {
+		log.Println(err)
+	}
+
+	tick := time.Tick(*interval)
+	for {
+		select {
+		case <-tick:
+			out, err := process(*mediaMin, *mediaMax, *tolerance, *activeTweet)
+			if err != nil {
+				out = []slack.Attachment{
+					{
+						Text: err.Error(),
+					},
+				}
+			}
+			if err := postSlack(webhook, slack.Attachments{Attachments: out}); err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
+
+func process(mediaMin, mediaMax, tolerance int, activeTweet time.Duration) (attachments []slack.Attachment, err error) {
 	val := url.Values{}
 	val.Add("screen_name", "gong023")
 	ret := <-cli.GetFriendsListAll(val)
 	if ret.Error != nil {
-		log.Fatal(ret.Error)
+		return attachments, ret.Error
 	}
 	favs, _ := cli.GetFavorites(val)
 	for _, fav := range favs {
 		ignore = append(ignore, fav.Id)
 	}
 	for _, friend := range ret.Friends {
-		active, err := isActiveUser(*activeTweet, friend.ScreenName)
+		active, err := isActiveUser(activeTweet, friend.ScreenName)
 		if err != nil {
-			log.Fatal(err)
+			return attachments, err
 		}
 		if !active {
 			continue
@@ -65,35 +105,28 @@ func main() {
 		val.Add("screen_name", friend.ScreenName)
 		favs, err := cli.GetFavorites(val)
 		if err != nil {
-			log.Fatal(err)
+			return attachments, err
 		}
 		for _, fav := range favs {
-			if !isTarget(*tolerance, *mediaMin, *mediaMax, fav) {
+			if !isTarget(tolerance, mediaMin, mediaMax, fav) {
 				continue
 			}
-			attachments := slack.Attachments{
-				Attachments: []slack.Attachment{
-					{
-						Fallback:   fmt.Sprintf("https://twitter.com/%s/status/%d", fav.User.ScreenName, fav.Id),
-						Pretext:    fmt.Sprintf("https://twitter.com/%s/status/%d", fav.User.ScreenName, fav.Id),
-						AuthorName: fmt.Sprintf("%s likes %s. count:%d", friend.ScreenName, fav.User.ScreenName, fav.FavoriteCount),
-						Text:       fav.FullText,
-					},
-				},
-			}
+			attachments = append(attachments, slack.Attachment{
+				Fallback:   fmt.Sprintf("https://twitter.com/%s/status/%d", fav.User.ScreenName, fav.Id),
+				Pretext:    fmt.Sprintf("https://twitter.com/%s/status/%d", fav.User.ScreenName, fav.Id),
+				AuthorName: fmt.Sprintf("%s likes %s. count:%d", friend.ScreenName, fav.User.ScreenName, fav.FavoriteCount),
+				Text:       fav.FullText,
+			})
 			for _, media := range fav.Entities.Media {
-				attachments.Attachments = append(attachments.Attachments, slack.Attachment{
+				attachments = append(attachments, slack.Attachment{
 					Title:     media.Url,
 					TitleLink: media.Url,
 					ImageURL:  media.Media_url_https,
 				})
 			}
-
-			if b, err := json.Marshal(attachments); err == nil {
-				fmt.Println(string(b))
-			}
 		}
 	}
+	return attachments, nil
 }
 
 func isActiveUser(activeTweet time.Duration, screenName string) (bool, error) {
@@ -129,4 +162,24 @@ func isTarget(tolerance, mediaMin, mediaMax int, favTweet anaconda.Tweet) bool {
 		return true
 	}
 	return false
+}
+
+func postSlack(webhook string, attachment slack.Attachments) (err error) {
+	sm, err := json.Marshal(attachment)
+
+	if err != nil {
+		return
+	}
+
+	res, err := http.Post(webhook, "application/json", bytes.NewBuffer(sm))
+
+	if err != nil {
+		return
+	}
+
+	if res.StatusCode >= 300 {
+		return errors.New("fail to post slack:" + res.Status)
+	}
+
+	return
 }
