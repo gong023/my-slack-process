f528badb02d44c90be73bfeed61f096bd4a0901d
gong023 <gon.gong.gone@gmail.com>
2019-06-15 19:48:21 -0700

    build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 BUILD_DIR=$(cd $(dirname $0); pwd)
-packages=(stdpost stdpostb stdpostc forecast coinbase hibiki inoreader pixivf pixivr twitterf)
+packages=(stdpost stdpostb stdpostc forecast coinbase hibiki inoreader pixivf pixivr twitterf onsen)
 for package in "${packages[@]}"; do
     cd "$BUILD_DIR"/"$package"
     GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o /usr/local/bin/"$package" github.com/gong023/my-slack-process/"$package"
